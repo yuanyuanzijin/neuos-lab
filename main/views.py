5b2b861362ea405d1769e665714d1cf2da4b6a2d
Louie Jin <jinluyuan@vip.qq.com>
2017-08-15 22:46:32 +0800

    0815-22

--- main/views.py
+++ main/views.py
@@ -3,18 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template.loader import get_template
 from django_cas_ng import views as baseviews
 from .models import User
 
 # Create your views here.
 def index(request):
-    template = get_template('index.html')
-    students = User.objects.all()
-    student_lists = list()
-    if request.user.is_authenticated():
-        print request.user.get_username()
-    else:
-        print 'not login'
+    template = get_template('login.html')
+    return HttpResponse(template.render(locals()))
 
+def home(request):
+    # 如果用户未登录则返回首页
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/')
+
+    template = get_template('home.html')
     return HttpResponse(template.render(locals()))
--- neuos/urls.py
+++ neuos/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from main.views import index
+from main.views import index, home
 import django_cas_ng
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'^home/', home),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login$', django_cas_ng.views.login, name='cas_ng_login'),
     url(r'^accounts/logout$', django_cas_ng.views.logout, name='cas_ng_logout'),
