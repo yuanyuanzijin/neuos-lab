71491097c44c641eaf37f59078e44ba386416e60
Louie Jin <jinluyuan@vip.qq.com>
2017-08-27 19:57:40 +0800

    开发版本修复wsgi

--- neuos/wsgi.py
+++ neuos/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neuos.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
